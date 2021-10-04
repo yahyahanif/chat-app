6edf1e0c647fec7db824572920841b3b8d105338
Amar Kamalapuri <55135751+AmarKamalapuri@users.noreply.github.com>
2021-10-04 20:34:24 +0530

    Update Header.jsx

--- client/src/Layout/Header.jsx
+++ client/src/Layout/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
