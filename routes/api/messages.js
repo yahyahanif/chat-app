dc7549a7bd94c03cbb8ff9c5f9b872b5ab2487d9
Amar Kamalapuri <55135751+AmarKamalapuri@users.noreply.github.com>
2021-10-04 18:34:10 +0530

    Update messages.js

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -19,7 +19,7 @@ router.use(function(req, res, next) {
     } catch (err) {
         console.log(err);
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'Unauthorized' }));
+        res.end(JSON.stringify({ message: 'Unauthorized error' }));
         res.sendStatus(401);
     }
 });
